5cb7fa4821e221a97ca0d85750ec96d998f4292a
TalhaNadeem284 <talhanadeemwahga43@gmail.com>
2022-01-10 13:05:49 +0500

    update url

--- config/db.js
+++ config/db.js
@@ -24,7 +24,7 @@ const mongoose = require('mongoose');
 
 const config = require('config');
 const password = "test1234"
-const db = `mongodb://TaskView:Task123@cluster0-shard-00-00.nbdrb.mongodb.net:27017,cluster0-shard-00-01.nbdrb.mongodb.net:27017,cluster0-shard-00-02.nbdrb.mongodb.net:27017/test?authSource=admin&replicaSet=atlas-10btdf-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`;
+const db = `mongodb+srv://ecomProject:PwcfE_dMTuH.7fq@cluster0.ofdid.mongodb.net/ecom?authSource=admin&replicaSet=atlas-rtdk6d-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`;
 console.log("deb",db)
 const connectDB = async () => {
     try {
